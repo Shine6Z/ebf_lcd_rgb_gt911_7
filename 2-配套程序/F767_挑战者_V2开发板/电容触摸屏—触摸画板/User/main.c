f75b585c6d7181898c12f1ab72829ffd287d0aa8
李桂佳 <571938783@qq.com>
2019-08-09 15:44:24 +0800

    7寸电容屏测试代码

---
+++ 2-配套程序/F767_挑战者_V2开发板/电容触摸屏—触摸画板/User/main.c
@@ -0,0 +1,147 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2016-xx-xx
+  * @brief   电容触摸屏-触摸画板
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32F767 开发板  
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+  
+#include "stm32f7xx.h"
+#include "./led/bsp_led.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./lcd/bsp_lcd.h"
+#include <stdlib.h>
+#include "main.h"
+#include "./touch/gt9xx.h"
+#include "./touch/palette.h"
+
+void Delay(__IO uint32_t nCount); 
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+  /* 系统时钟初始化成216 MHz */
+  SystemClock_Config();
+  /* LED 端口初始化 */
+  LED_GPIO_Config();
+  /* 串口初始化 */
+  DEBUG_USART_Config();
+  /* 初始化触摸屏 */
+  GTP_Init_Panel(); 	
+  /* LCD 端口初始化 */ 
+  LCD_Init();
+  /* LCD 第二层初始化 */ 
+  LCD_LayerInit(1, LCD_FB_START_ADDRESS+(LCD_GetXSize()*LCD_GetYSize()*4),ARGB8888);
+  /* 使能LCD，包括开背光 */ 
+  LCD_DisplayOn(); 
+
+  /* 选择LCD第二层 */
+  LCD_SelectLayer(1);
+
+  /* 第二层清屏，显示全黑 */ 
+  LCD_Clear(LCD_COLOR_TRANSPARENT);
+
+  /* 配置透明度,最小值为0，最大值为255*/
+
+  LCD_SetTransparency(1, 255);
+  printf("\r\n野火STM3F767 触摸画板测试例程\r\n");
+  /*调用画板函数*/
+  Palette_Init();
+
+  Delay(0xfff);
+
+  while(1)
+  {
+
+  }
+}
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下 : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 432
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 9
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 7
+  * @param  无
+  * @retval 无
+  */
+void SystemClock_Config(void)
+{
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	HAL_StatusTypeDef ret = HAL_OK;
+
+	/* 使能HSE，配置HSE为PLL的时钟源，配置PLL的各种分频因子M N P Q 
+	 * PLLCLK = HSE/M*N/P = 25M / 25 *432 / 2 = 216M
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 25;
+	RCC_OscInitStruct.PLL.PLLN = 432;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+
+	ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}
+
+	/* 激活 OverDrive 模式以达到216M频率  */  
+	ret = HAL_PWREx_EnableOverDrive();
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}
+
+	/* 选择PLLCLK作为SYSCLK，并配置 HCLK, PCLK1 and PCLK2 的时钟分频因子 
+	 * SYSCLK = PLLCLK     = 216M
+	 * HCLK   = SYSCLK / 1 = 216M
+	 * PCLK2  = SYSCLK / 2 = 108M
+	 * PCLK1  = SYSCLK / 4 = 54M
+	 */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2; 
+
+	ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7);
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}  
+}
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+/*********************************************END OF FILE**********************/
+
